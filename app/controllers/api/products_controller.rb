28da6bfd82146f5fa58b7819cba4ce17bbabc1f7
Kyle Hassett <kylehassett72@gmail.com>
2018-08-30 21:15:08 -0500

    Added Patch and Destroy

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,11 +4,6 @@ class Api::ProductsController < ApplicationController
     render 'index.json.jbuilder'
   end
 
-  def show
-    @hammer = Product.find(params[:id])
-    render 'show.json.jbuilder'
-  end
-
   def create
     @hammer = Product.new(
                           name: params[:name],
@@ -18,5 +13,28 @@ class Api::ProductsController < ApplicationController
                           )
     @hammer.save
     render 'show.json.jbuilder'
+  end
+
+  def show
+    @hammer = Product.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @hammer = Product.find(params[:id])
+
+    @hammer.name = params[:name] || @hammer.name
+    @hammer.price = params[:name] || @hammer.price
+    @hammer.image_url = params[:name] || @hammer.image_url
+    @hammer.description = params[:name] || @hammer.description
+
+    @hammer.save
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    @hammer = Product.find(params[:id])
+    @hammer.destroy
+    render json: {message: "Player successfully destroyed"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/hammers" => 'products#index'
-    get "/hammers/:id" => 'products#show'
     post "/hammers" => 'products#create'
+    get "/hammers/:id" => 'products#show'
+    patch "/hammers/:id" => 'products#update'
+    delete "/hammers/:id" => 'products#destroy'
   end
 end
