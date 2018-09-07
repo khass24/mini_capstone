cb69621d9a339192e47f51e62322d1f3e53c1aed
Kyle Hassett <kylehassett72@gmail.com>
2018-09-06 21:26:44 -0500

    Added suppliers model in class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,14 @@
-:class Product < ApplicationRecord
+class Product < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
-  validates :description, length: {minimum: 20}
-  validates :description, length: {maximum: 200}
+  validates :description, length: {minimum: 1}
+  validates :description, length: {maximum: 100}
   validates :description, presence: true
   validates :image_url, uniqueness: true
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
+
+  belongs_to :supplier
 
   def is_discounted?
     price < 7000000
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_021910) do
+ActiveRecord::Schema.define(version: 2018_09_07_004733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2018_09_04_021910) do
     t.decimal "price", precision: 11, scale: 2
     t.string "image_url"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
